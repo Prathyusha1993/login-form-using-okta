83bd98f3ede47a03b77c0cb2d40f5828b9eb7da3
unknown <pkr131023@gmail.com>
2020-09-22 14:12:56 -0700

    added security secureRoute and implicitcallback

--- src/App.js
+++ src/App.js
@@ -4,20 +4,34 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import Staff from './components/pages/Staff';
+import Login from './components/auth/Login';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
+
+
+function onAuthRequired({history}) {
+  history.push('/login');
+}
 
 class App extends Component {
   state = {  }
   render() { 
     return ( 
       <Router>
+        <Security issuer='https://dev-681928.okta.com/oauth2/default'
+                  clientId='0oa116re3nck6dyb14x7'
+                  redirectUri={window.location.origin + '/implicit/callback'}
+                  onAuthRequired={this.onAuthRequired}>
       <div className="App">
         <Navbar />
         <div className="container">
         <Route path="/" exact={true} component={Home} />
-        <Route path="/staff" exact={true} component={Staff} />
+        <SecureRoute path="/staff" exact={true} component={Staff} />
+        <Route path='/login' render={() => <Login baseUrl='https://dev-681928.okta.com' />} />
+        <Route path='/implicit/callback' component={ImplicitCallback} />
         </div>
       </div>
+      </Security>
       </Router>
      );
   }
