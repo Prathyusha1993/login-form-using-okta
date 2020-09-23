51613afe85c1d0079c6ee70aa2c2b73b36e2a989
unknown <pkr131023@gmail.com>
2020-09-22 17:23:47 -0700

    added few files using okta documentation like login siginwidget and home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { withOktaAuth } from '@okta/okta-react';
 
-export default () => {
-    return (
+export default withOktaAuth(class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+
+  async login() {
+    this.props.authService.login('/');
+  }
+
+  async logout() {
+    this.props.authService.logout('/');
+  }
+
+  render() {
+    if (this.props.authState.isPending) return null;
+
+    // const button = this.props.authState.isAuthenticated ?
+    //   <button onClick={this.logout}>Logout</button> :
+    //   <button onClick={this.login}>Login</button>;
+
+    const mainContent = this.props.authState.isAuthenticated ? (
         <div>
-            <h1>Home</h1>
+            <p className="lead"> You have entered the staff portal, <Link to="/staff">Click here</Link></p>
+            <button className="btn btn-light btn-lg" onClick={this.logout}>Logout</button>
         </div>
-    )
-}
+    ) : (
+        <div>
+            <p className="lead"> if you are staff member please get your ceredentials from supervisor</p>
+            <button className="btn btn-light btn-lg" onClick={this.login}>Login</button>
+        </div>
+    );
+
+    return (
+      <div className="jjumbotron">
+        <h1 className="display-4">Acme Staff portal</h1>
+        {mainContent}
+      </div>
+    );
+  }
+});
+
+{/* <div>
+        <Link to='/'>Home</Link><br/>
+        <Link to='/protected'>Protected</Link><br/>
+        {button}
+      </div> */}
--- src/components/pages/Staff.js
+++ src/components/pages/Staff.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-    return (
-        <div>
-            <h1>Staff</h1>
-        </div>
-    )
+class Staff extends Component {
+    state = { 
+        currentUserName : '',
+        currentUserEmail : ''
+     }
+
+     componentDidMount(){
+         const idToken = JSON.parse(localStorage.getItem('okta-token-storage'));
+         this.setState({
+             currentUserEmail: idToken.idToken.claims.email,
+             currentUserName : idToken.idToken.claims.name
+         });
+     }
+    render() { 
+        console.log(this.state);
+        const { currentUserName, currentUserEmail } = this.state;
+        return ( 
+            <div>
+                <h1>Welcome {currentUserName }</h1>
+                <p>Email : {currentUserEmail}</p>
+                <p>You have reached the authorizede staff area of te portal</p>
+            </div>
+         );
+    }
 }
+ 
+export default Staff;
